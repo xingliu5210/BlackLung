0d223837e87af0de61d43bf11c4c5e60e66d7f0d
xingliu5210 <liu.xing4@northeastern.edu>
2024-11-13 11:44:01 -0500

    modified gravity of jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,8 @@ public class PlayerMovement : MonoBehaviour
     protected float vertical;     // Vertical input for climbing
     protected bool isLadder;      // Indicates if the player is near a ladder
     protected bool isClimbing;    // Indicates if the player is currently climbing
+    private float upwardGravityScale = 1.5f;
+    private float downwardGravityScale = 7f;
 
     private void Awake()
     {
@@ -41,6 +43,18 @@ public class PlayerMovement : MonoBehaviour
         {
             // If not climbing, apply horizontal movement
             OnMove(moveInput);
+
+            if (!grounded)
+            {
+                if (body.velocity.y > 0) // Ascending
+                {
+                    body.velocity += Vector3.up * Physics.gravity.y * (upwardGravityScale - 1) * Time.deltaTime;
+                }
+                else if (body.velocity.y < 0) // Descending
+                {
+                    body.velocity += Vector3.up * Physics.gravity.y * (downwardGravityScale - 1) * Time.deltaTime;
+                }
+            }
         }
     }
 
