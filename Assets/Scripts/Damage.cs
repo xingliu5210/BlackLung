4da1625d09c84d27688613c6cf83d1523d37f31d
RJG-Klypze <randy.j.garcia942@gmail.com>
2024-11-14 17:00:15 -0500

    Added Fearbar/Healthbar

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //Checks if collides with player. Then, reduces health by damage set.
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().health -= damage;
+
+            if (damage < 0)
+            { Debug.Log("Player has recovered " + -damage + " health"); }
+            else
+            { Debug.Log("Player has taken " + damage + " damage"); }
+
+            other.gameObject.GetComponent<PlayerHealth>().Healthbar();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    [SerializeField] private Image healthBar;
+
+    public void Healthbar()
+    {
+        //Restrict Health to maximum/minimum
+        if (health > maxHealth)
+        { health = maxHealth; }
+
+        if (health < 0)
+        { health = 0; }
+
+        //Translate health values into health bar
+        healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+
+        Debug.Log("Player has " + health + "/" + maxHealth + "hp");
+
+        if (health <= 0)
+        { HealthDepleted(); }
+    }
+
+    private void HealthDepleted()
+    {
+        Debug.Log("HEALTH DEPLETED!");
+    }
+}
