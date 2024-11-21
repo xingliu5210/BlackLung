253c076960c0bb0dbc3f1089eacc43c8410d09c7
RJG-Klypze <randy.j.garcia942@gmail.com>
2024-11-21 02:18:03 -0500

    Lantern Update
    Gain 20% from pickups instead of 100%
    Lose 1% fuel every 1.2 seconds
    Touching water sets fuel to 0%

--- Assets/Scripts/Lantern.cs
+++ Assets/Scripts/Lantern.cs
@@ -4,35 +4,96 @@ using UnityEngine;
 
 public class Lantern : MonoBehaviour
 {
+    //private bool PowerOn = false;
+
     public Light Lightsource1;
     public Light Lightsource2;
 
-    [SerializeField] private float maxBrightness;
-    [SerializeField] private float fuelPerFrame;
+    private Color startColor = new Color(1.0f, 0.76f, 0.0f); //Lanterns original bright glow #FFC200
+    private Color endColor = Color.black; //Lantern gets dark as its light diminishes
+    private Color currentColor;
 
+    [SerializeField] private Material material;
+
+    private float maxBrightness;
+    [SerializeField] private float fuelUsagePercentPerSecond;
+    private float fuelUsage;
+
+    private float currentFuelPercent;
+
+    [SerializeField] private float refuelPercent;
+    private float refuelAmount;
+
+    private float updateRate = 50f;
+
+    private void Start()
+    {
+        maxBrightness = Lightsource1.intensity;
+
+        //Translate from percentage
+        fuelUsage = maxBrightness * (fuelUsagePercentPerSecond / 100 / updateRate);
+        refuelAmount = maxBrightness * (refuelPercent / 100);
+    }
     private void FixedUpdate()
     {
-        //Light reduction overtime
+        //Brightness maximum value
+        if (Lightsource1.intensity > maxBrightness)
+        { 
+            Lightsource1.intensity = maxBrightness;
+            Lightsource2.intensity = maxBrightness;
+        }
+
+        //Fuel burn per frame
         if (Lightsource1.intensity > 0)
         {
-            Lightsource1.intensity -= fuelPerFrame;
-            Lightsource2.intensity -= fuelPerFrame;
+            Lightsource1.intensity -= fuelUsage;
+            Lightsource2.intensity -= fuelUsage;
         }
         else if (Lightsource1.intensity < 0)
         { 
             Lightsource1.intensity = 0;
             Lightsource2.intensity = 0;
         }
+        else
+        {
+            //PowerOn = false;
+        }
+
+        //The lamps color value changes with its brightness output
+        currentFuelPercent = Lightsource1.intensity / maxBrightness;
+        currentColor = Color.Lerp(endColor, startColor, currentFuelPercent);
+
+        //material.color = currentColor;
+        material.SetColor("_EmissionColor", currentColor);
     }
+
     private void OnTriggerEnter(Collider other)
     {
+        //Water depletes fuel
+        if (other.CompareTag("Water"))
+        {
+            Lightsource1.intensity = 0;
+            Lightsource2.intensity = 0;
+        }
+
+        //Remove this below once Refuel() function works with inventory
         if (other.CompareTag("Pickup"))
         {
-            //Reset light intensity with fuel pickup
+            Refuel();
+            Destroy(other.gameObject);
+        }
+    }
+
+    //Add 20% fuel to lantern
+    public void Refuel()
+    {
+        Lightsource1.intensity += refuelAmount;
+        Lightsource2.intensity += refuelAmount;
+
+        if (Lightsource1.intensity > maxBrightness)
+        {
             Lightsource1.intensity = maxBrightness;
             Lightsource2.intensity = maxBrightness;
-
-            Destroy(other.gameObject);
         }
     }
 }
