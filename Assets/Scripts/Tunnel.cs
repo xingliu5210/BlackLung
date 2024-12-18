06cb91ff6b963b11c628f9272d6acddbea520e43
RJG-Klypze <randy.j.garcia942@gmail.com>
2024-12-18 05:07:32 -0500

    Tunnels Added
    Bo can use these wormhole tunnels to fast travel.
    
    TunnelPair prefab included
    
    Amos and Bo no longer collide with each other

--- Assets/Scripts/Tunnel.cs
+++ Assets/Scripts/Tunnel.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Minecart : MonoBehaviour
+public class Tunnel : MonoBehaviour
+
 {
-    public GameObject PairedCart;
+    public GameObject PairedTunnel;
 }
---
+++ Assets/Scripts/TunnelCrawl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelCrawl : MonoBehaviour
+{
+    public int tunnelCooldown;
+    private int counter = 0;
+
+    bool inCooldown = false;
+
+    private void FixedUpdate()
+    {
+        if (inCooldown == true)
+        { Cooldown();}
+    }
+
+    void Cooldown()
+    {
+        counter++;
+
+        if (counter >= tunnelCooldown)
+        {
+            counter = 0;
+            inCooldown = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Tunnel") && !inCooldown)
+        {
+            Debug.Log("TUNNEL!");
+            transform.position = col.GetComponent<Tunnel>().PairedTunnel.transform.position;
+            inCooldown = true;
+        }
+    }
+}
