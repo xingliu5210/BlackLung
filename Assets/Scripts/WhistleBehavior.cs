01be616d39c2b57a562072846b44783df70ccb51
xingliu5210 <liu.xing4@northeastern.edu>
2025-06-19 11:02:23 -0400

    Added Raycast

--- Assets/Scripts/WhistleBehavior.cs
+++ Assets/Scripts/WhistleBehavior.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WhistleBehavior : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Camera amosCamera;     // Assign Camera_Amos
     [SerializeField] private GameObject bo;         // Assign Bo object
+    [SerializeField] private float raycastDistance = 50f;
+    [SerializeField] private float navmeshSampleRadius = 2f;
 
     private CharacterSwitcher characterSwitcher;
     private AmosControls amosControls;
@@ -28,6 +31,7 @@ public class WhistleBehavior : MonoBehaviour
             if (characterSwitcher.GetControlledCharacter() == amosControls)
             {
                 CheckBoVisibility();
+                TryWhistleSpawn();
             }
         }
     }
@@ -45,5 +49,49 @@ public class WhistleBehavior : MonoBehaviour
         bool isVisible = GeometryUtility.TestPlanesAABB(planes, boRenderer.bounds);
 
         Debug.Log(isVisible ? "Bo in Screen" : "Bo is off screen");
+    }
+
+    private void TryWhistleSpawn()
+    {
+        float camZDistance = Mathf.Abs(amosCamera.transform.position.z - transform.position.z);
+
+        // Viewport points slightly outside left and right
+        Vector3[] viewportPoints = new Vector3[]
+        {
+            new Vector3(-0.1f, 0.5f, camZDistance),  // left
+            new Vector3(1.1f, 0.5f, camZDistance),   // right
+        };
+
+        foreach (var vp in viewportPoints)
+        {
+            Vector3 worldPoint = amosCamera.ViewportToWorldPoint(vp);
+
+            if (CheckRayHitNavMesh(worldPoint, Vector3.up) ||
+                CheckRayHitNavMesh(worldPoint, Vector3.down))
+            {
+                Debug.Log("Bo can be spawned");
+                return;
+            }
+        }
+
+        Debug.Log("Bo can't be safely spawned");
+    }
+
+    private bool CheckRayHitNavMesh(Vector3 origin, Vector3 direction)
+    {
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, raycastDistance))
+        {
+            if (NavMesh.SamplePosition(hit.point, out NavMeshHit navHit, navmeshSampleRadius, NavMesh.AllAreas) &&
+                NavMesh.SamplePosition(transform.position, out NavMeshHit amosNavHit, navmeshSampleRadius, NavMesh.AllAreas))
+            {
+                // Compare area masks OR closeness in position
+                if (Vector3.Distance(navHit.position, amosNavHit.position) < 10f)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
